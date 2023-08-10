b0b06c8da541b6ad37c805fa41d1df970112805f
TheOreoTM <qwertzybad@gmail.com>
2023-08-10 23:54:45 +0500

    start of forum tracker

---
+++ src/listeners/support/forum.ts
@@ -0,0 +1,7 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener } from '@sapphire/framework';
+
+@ApplyOptions<Listener.Options>({})
+export class UserEvent extends Listener {
+	public override run() {}
+}
