6abbc8b549b09fd1bf21e251a3e3d5a9cc352cf4
TheOreoTM <qwertzybad@gmail.com>
2023-08-22 21:17:50 +0500

    duration command

--- src/commands/Moderation/duration.ts
+++ src/commands/Moderation/duration.ts
@@ -1,7 +1,8 @@
 import { CardinalEmbedBuilder, ModerationCommand } from '#lib/structures';
-import { CardinalEmojis } from '#utils/constants';
+import { ModerationType } from '#utils/moderationConstants';
 import { ApplyOptions } from '@sapphire/decorators';
 import { send } from '@sapphire/plugin-editable-commands';
+import { DurationFormatter } from '@sapphire/time-utilities';
 
 @ApplyOptions<ModerationCommand.Options>({
 	description: 'Change the duration of a moderation',
@@ -14,27 +15,66 @@ import { send } from '@sapphire/plugin-editable-commands';
 })
 export class durationCommand extends ModerationCommand {
 	public override async messageRun(message: ModerationCommand.Message, args: ModerationCommand.Args) {
-		if (message.guild) {
-			return send(message, {
-				content: `${CardinalEmojis.Info} *Coming soon :tm:*`
-			});
-		}
-		const target = await args.pick('member').catch(() => null);
+		const caseId = await args.pick('number').catch(() => null);
 
-		if (!target) {
+		if (!caseId) {
 			return send(message, {
-				embeds: [new CardinalEmbedBuilder().setStyle('fail').setDescription('Provide a valid member to warn')]
+				embeds: [new CardinalEmbedBuilder().setStyle('fail').setDescription('Provide a valid modlog ID (mute/ban)')]
 			});
 		}
 
-		const reason = await args.rest('string').catch(() => null);
+		const newDuration = await args.rest('duration').catch(() => null);
 
-		if (!reason) {
+		if (!newDuration) {
 			return send(message, {
-				embeds: [new CardinalEmbedBuilder().setStyle('fail').setDescription('Provide a valid reason')]
+				embeds: [new CardinalEmbedBuilder().setStyle('fail').setDescription('Provide a valid duration')]
 			});
 		}
 
-		return;
+		const caseData = await this.container.db.modlog.update({
+			where: {
+				id: caseId,
+				type: {
+					in: [ModerationType.Mute, ModerationType.Ban]
+				}
+			},
+			data: {
+				length: new DurationFormatter().format(newDuration.offset)
+			}
+		});
+
+		if (!caseData) {
+			return send(message, {
+				embeds: [
+					new CardinalEmbedBuilder()
+						.setStyle('fail')
+						.setDescription('A modlog of that id doesnt exist, make sure its either a mute or a ban.')
+				]
+			});
+		}
+
+		try {
+			await this.container.db.mute.updateMany({
+				where: {
+					modlogId: caseData.id
+				},
+				data: {
+					expiresAt: newDuration.fromNow
+				}
+			});
+
+			await this.container.db.ban.updateMany({
+				where: {
+					modlogId: caseData.id
+				},
+				data: {
+					expiresAt: newDuration.fromNow
+				}
+			});
+		} catch (ignored) {}
+
+		return send(message, {
+			embeds: [new CardinalEmbedBuilder().setStyle('success').setDescription(`Updated duration for #${caseId}`)]
+		});
 	}
 }
