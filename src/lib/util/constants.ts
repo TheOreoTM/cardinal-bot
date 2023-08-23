840326ff3dc843c9036d49de9eb3a9bdcbaca40f
TheOreoTM <qwertzybad@gmail.com>
2023-08-23 17:01:41 +0500

    d

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -12,7 +12,7 @@ export const BotPrivilegedUsers = envParseArray('BOT_PRIVILEGED_USERS');
 export const CooldownFiltered = [BotOwner, ...BotPrivilegedUsers];
 export const BotPrefix = envParseString('BOT_PREFIX');
 export const BotClientID = '740962735306702858';
-export const BotVersion = '1.2.1';
+export const BotVersion = '1.2.2';
 
 export const MainServerID = '519734247519420438';
 /**
