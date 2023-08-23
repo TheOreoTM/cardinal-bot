bcb5ff1291e89cfb2127a228a915f576445b90d0
TheOreoTM <qwertzybad@gmail.com>
2023-08-23 17:52:33 +0500

    lord forgive me

--- src/commands/Private/clink.ts
+++ src/commands/Private/clink.ts
@@ -1,6 +1,7 @@
 import { CardinalCommand } from '#lib/structures';
 import { ApplyOptions } from '@sapphire/decorators';
-import { send } from '@sapphire/plugin-editable-commands';
+import { FetchResultTypes, fetch } from '@sapphire/fetch';
+import { reply, send } from '@sapphire/plugin-editable-commands';
 
 @ApplyOptions<CardinalCommand.Options>({
 	description: 'Clink is a...',
@@ -21,25 +22,25 @@ export class clinkCommand extends CardinalCommand {
 			return send(message, praiseMessage);
 		}
 
-		const randomInsult = insults[Math.floor(Math.random() * insults.length)];
-		const insultMessage = `${randomInsult.replace('${user}', `<@${message.member.id}>`)}`;
-		return send(message, insultMessage);
+		const randomInsult: { insult: string } = await fetch('https://evilinsult.com/generate_insult.php?&type=json', FetchResultTypes.JSON);
+		const insultMessage = randomInsult.insult;
+		return reply(message, insultMessage);
 	}
 }
-const insults = [
-	'${user} is so bad at parking, they could win an award for it!',
-	"Did you hear about ${user}'s cooking? They say it's a great way to start a fire.",
-	"If ${user} were any clumsier, they'd trip over a wireless network.",
-	'I told ${user} they\'re "one in a million." They took it as an insult.',
-	"Is ${user} a baker? Because they can't seem to make enough dough.",
-	'If brains were money, ${user} would be in serious debt.',
-	'${user} is the reason the gene pool needs a lifeguard.',
-	'Last time ${user} had a bright idea, it was just a reflection.',
-	"${user} doesn't need a sunscreen. They're already in the shade of intelligence.",
-	'Why did ${user} bring a ladder to the bar? Because they heard the drinks were on the house.',
-	"Did you hear about ${user}'s selfie addiction? It's just their way of trying to improve their looks.",
-	"${user}'s sense of direction must be keyboard shortcuts, because they can't find their way without them."
-];
+// const insults = [
+// 	'${user} is so bad at parking, they could win an award for it!',
+// 	"Did you hear about ${user}'s cooking? They say it's a great way to start a fire.",
+// 	"If ${user} were any clumsier, they'd trip over a wireless network.",
+// 	'I told ${user} they\'re "one in a million." They took it as an insult.',
+// 	"Is ${user} a baker? Because they can't seem to make enough dough.",
+// 	'If brains were money, ${user} would be in serious debt.',
+// 	'${user} is the reason the gene pool needs a lifeguard.',
+// 	'Last time ${user} had a bright idea, it was just a reflection.',
+// 	"${user} doesn't need a sunscreen. They're already in the shade of intelligence.",
+// 	'Why did ${user} bring a ladder to the bar? Because they heard the drinks were on the house.',
+// 	"Did you hear about ${user}'s selfie addiction? It's just their way of trying to improve their looks.",
+// 	"${user}'s sense of direction must be keyboard shortcuts, because they can't find their way without them."
+// ];
 
 const compliments = [
 	'${user} is incredibly kind-hearted and compassionate.',
