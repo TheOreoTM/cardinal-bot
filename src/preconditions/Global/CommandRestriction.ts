c84f11ad1e741ff2f71ff8876d0cb7103ad92dd4
TheOreoTM <qwertzybad@gmail.com>
2023-08-27 22:11:46 +0500

    testing in prod

--- src/preconditions/Global/CommandRestriction.ts
+++ src/preconditions/Global/CommandRestriction.ts
@@ -6,7 +6,7 @@ import { Precondition, type MessageCommand, type ChatInputCommand, type ContextM
 import type { ChatInputCommandInteraction, ContextMenuCommandInteraction, Message } from 'discord.js';
 
 @ApplyOptions<Precondition.Options>({
-	position: 3,
+	position: 1,
 	enabled: true
 })
 export class UserPrecondition extends Precondition {
@@ -39,10 +39,13 @@ export class UserPrecondition extends Precondition {
 
 		if (!channel) return this.ok();
 
+		console.log(member.roles.cache);
+
 		const memberIsAllowed = await guild.settings.restrictions.checkMemberAllowed(command.name, member.id);
 		const channelIsAllowed = await guild.settings.restrictions.checkChannelAllowed(command.name, channel.id);
 		const roleIsAllowed = await guild.settings.restrictions.checkRoleAllowed(command.name, member.roles.cache);
 
+		console.log('hey', memberIsAllowed, channelIsAllowed, roleIsAllowed);
 		// console.log(!memberIsAllowed, await isTrainee(member), !(command instanceof ModerationCommand));
 
 		if (memberIsAllowed === false || channelIsAllowed === false || roleIsAllowed === false)
