eb98242e78228550550b4e120b3a27c39ac5dcc3
TheOreoTM <qwertzybad@gmail.com>
2023-10-03 02:13:48 +0500

    caching?

--- src/lib/CardinalClient.ts
+++ src/lib/CardinalClient.ts
@@ -1,10 +1,31 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { Enumerable } from '@sapphire/decorators';
 import { SapphireClient, container, type SapphirePrefix, type SapphirePrefixHook } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 import { ClientConfig } from '#config';
 import { LongLivingReactionCollector } from '#utils/LongLivingReactionCollector';
 import { BotPrefix } from '#constants';
+import Redis from 'ioredis';
+import { createPrismaRedisCache } from 'prisma-redis-middleware';
+
+const redis = new Redis();
+const prisma = new PrismaClient();
+const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+	models: [{ model: 'Message', cacheTime: 300 }],
+	storage: { type: 'redis', options: { client: redis, invalidation: { referencesTTL: 300 }, log: console } },
+	cacheTime: 300,
+	onHit: (key) => {
+		console.log('hit', key);
+	},
+	onMiss: (key) => {
+		console.log('miss', key);
+	},
+	onError: (key) => {
+		console.log('error', key);
+	}
+});
+
+prisma.$use(cacheMiddleware);
 
 export class CardinalClient<Ready extends boolean = boolean> extends SapphireClient<Ready> {
 	@Enumerable(false)
@@ -15,7 +36,7 @@ export class CardinalClient<Ready extends boolean = boolean> extends SapphireCli
 	}
 
 	public override async login(token?: string): Promise<string> {
-		container.db = new PrismaClient();
+		container.db = prisma;
 		return super.login(token);
 	}
 	public override destroy() {
