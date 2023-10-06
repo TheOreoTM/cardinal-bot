db11a08459ca3eb40a39fff9e24f797bdc62fb83
TheOreoTM <qwertzybad@gmail.com>
2023-10-06 22:08:35 +0500

    punching

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -6,7 +6,7 @@ import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plu
 })
 export class UserRoute extends Route {
 	public [methods.GET](request: ApiRequest, response: ApiResponse) {
-		console.log(request.auth);
+		console.log(JSON.stringify(request), request.auth);
 		response.json({ message: 'I am alive' });
 	}
 
