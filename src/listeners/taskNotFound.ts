fdb120d67309d129c06cb550ab631972b8646041
TheOreoTM <qwertzybad@gmail.com>
2023-11-05 19:43:20 +0500

    open soource

--- src/listeners/taskNotFound.ts
+++ src/listeners/taskNotFound.ts
@@ -4,7 +4,7 @@ import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks';
 
 @ApplyOptions<Listener.Options>({ event: ScheduledTaskEvents.ScheduledTaskNotFound })
 export class UserEvent extends Listener {
-	public override run() {
-		console.log('not found');
+	public override run(name: string) {
+		console.log('not found', name);
 	}
 }
--- src/listeners/taskRun.ts
+++ src/listeners/taskRun.ts
@@ -4,7 +4,7 @@ import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks';
 
 @ApplyOptions<Listener.Options>({ event: ScheduledTaskEvents.ScheduledTaskRun })
 export class UserEvent extends Listener {
-	public override run() {
-		console.log('run');
+	public override run(name: string) {
+		console.log('run', name);
 	}
 }
