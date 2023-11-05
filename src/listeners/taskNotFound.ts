028b85b5521707e4712732631930e69ef51aa59f
TheOreoTM <qwertzybad@gmail.com>
2023-11-05 19:39:00 +0500

    yeahhhh

---
+++ src/listeners/taskNotFound.ts
@@ -0,0 +1,10 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener } from '@sapphire/framework';
+import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks';
+
+@ApplyOptions<Listener.Options>({ event: ScheduledTaskEvents.ScheduledTaskNotFound })
+export class UserEvent extends Listener {
+	public override run() {
+		console.log('not found');
+	}
+}
---
+++ src/listeners/taskRun.ts
@@ -0,0 +1,10 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener } from '@sapphire/framework';
+import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks';
+
+@ApplyOptions<Listener.Options>({ event: ScheduledTaskEvents.ScheduledTaskRun })
+export class UserEvent extends Listener {
+	public override run() {
+		console.log('run');
+	}
+}
