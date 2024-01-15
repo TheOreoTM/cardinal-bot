1d91eb5f66460aa5a0ab0ea26e71e15f2712fe08
theoreotm <qwertzybad@gmail.com>
2024-01-15 16:19:01 +0500

    style(automod): Moved markdown formatting so that its always triggered

--- src/commands/Admin/automod.ts
+++ src/commands/Admin/automod.ts
@@ -474,7 +474,7 @@ export class automodCommand extends ModerationCommand {
 					name: 'Automute Settings',
 					value: [
 						`Automute After: \`${data?.automuteAfter ? `${data.automuteAfter} Infractions` : '-'}\`` ?? 'None',
-						`Automute For: __${data?.automuteDuration ? `${new DurationFormatter().format(data.automuteDuration)}__` : '-'}` ?? 'None'
+						`Automute For: __${data?.automuteDuration ? `${new DurationFormatter().format(data.automuteDuration)}` : '-'}__` ?? 'None'
 					].join('\n')
 				},
 				{
