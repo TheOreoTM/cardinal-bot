d84c894644eaf2ae008c4e5344007d81c0d8ad82
theoreotm <qwertzybad@gmail.com>
2024-02-05 23:54:24 +0500

    hotfix(metrics): Use the correct event

--- src/listeners/analytics/guilds/guildCreateAnalytics.ts
+++ src/listeners/analytics/guilds/guildCreateAnalytics.ts
@@ -2,7 +2,7 @@ import { CardinalEvents } from '#lib/types';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
 
-@ApplyOptions<Listener.Options>({ event: CardinalEvents.MessageCreate })
+@ApplyOptions<Listener.Options>({ event: CardinalEvents.GuildCreate })
 export class UserAnalyticsEvent extends Listener {
 	public run(): void {
 		this.container.client.analytics.addGuild();
