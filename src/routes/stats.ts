3221a80c627969d9cae8af2875a2e79f8d88d74c
theoreotm <qwertzybad@gmail.com>
2024-02-13 21:02:00 +0500

    feat(api): Show basic stats for dashboard usage

---
+++ src/routes/stats.ts
@@ -0,0 +1,19 @@
+import { ratelimit } from '#lib/api/util';
+import { seconds } from '#utils/common';
+import { ApplyOptions } from '@sapphire/decorators';
+import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+
+@ApplyOptions<Route.Options>({
+	route: 'stats'
+})
+export class UserRoute extends Route {
+	@ratelimit(seconds(5), 2, false)
+	public [methods.GET](_request: ApiRequest, response: ApiResponse) {
+		const guilds = this.container.client.guilds.cache.size;
+		const members = this.container.client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+		const channels = this.container.client.channels.cache.size;
+		const commands = this.container.db.command.count();
+
+		return response.json({ guildAmount: guilds, memberAmount: members, channelAmount: channels, commandsUsed: commands });
+	}
+}
