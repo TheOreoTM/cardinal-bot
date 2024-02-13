edc648a9d8d5ae63aa61115d8fec2dd6c611495d
theoreotm <qwertzybad@gmail.com>
2024-02-13 21:06:12 +0500

    fix: Await command count promise

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -8,11 +8,11 @@ import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plu
 })
 export class UserRoute extends Route {
 	@ratelimit(seconds(5), 2, false)
-	public [methods.GET](_request: ApiRequest, response: ApiResponse) {
+	public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
 		const guilds = this.container.client.guilds.cache.size;
 		const members = this.container.client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
 		const channels = this.container.client.channels.cache.size;
-		const commands = this.container.db.command.count();
+		const commands = await this.container.db.command.count();
 
 		return response.json({ guildAmount: guilds, memberAmount: members, channelAmount: channels, commandsUsed: commands });
 	}
