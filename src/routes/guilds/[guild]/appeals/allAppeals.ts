ab2f5e192adc0579ce706e46cc236ca726cec067
theoreotm <qwertzybad@gmail.com>
2024-02-23 18:17:05 +0500

    feat(api): Appeal routes

---
+++ src/routes/guilds/[guild]/appeals/allAppeals.ts
@@ -0,0 +1,21 @@
+import { authenticated } from '#lib/api/util';
+import { ApplyOptions } from '@sapphire/decorators';
+import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+
+@ApplyOptions<Route.Options>({
+	route: 'guilds/:guild/appeals'
+})
+export class UserRoute extends Route {
+	@authenticated()
+	public async [methods.GET](request: ApiRequest, response: ApiResponse) {
+		const guildId = request.params.guild;
+
+		const appeals = await this.container.db.appeal.findMany({
+			where: {
+				guildId
+			}
+		});
+
+		return response.json({ ...appeals });
+	}
+}
