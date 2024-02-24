4861a0a880bb6fb566b121bd8607561d85c0c220
theoreotm <qwertzybad@gmail.com>
2024-02-24 11:28:27 +0500

    feat(api): Send appeal embed

--- src/routes/appeals/postAppeal.ts
+++ src/routes/appeals/postAppeal.ts
@@ -1,7 +1,9 @@
 import { authenticated } from '#lib/api/util';
+import { CardinalColors, CardinalEmojis } from '#utils/constants';
 import { ApplyOptions } from '@sapphire/decorators';
 import { methods, Route, type ApiRequest, type ApiResponse, HttpCodes } from '@sapphire/plugin-api';
 import { s } from '@sapphire/shapeshift';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, type TextChannel } from 'discord.js';
 
 @ApplyOptions<Route.Options>({
 	name: 'postAppeal',
@@ -35,6 +37,67 @@ export class UserRoute extends Route {
 				idx: nextIdx,
 				...data
 			}
+		});
+
+		const guildData = await this.container.db.guild.findUnique({
+			where: {
+				guildId: appeal.guildId
+			}
+		});
+
+		if (!guildData?.channelAppeal) {
+			return response.error(HttpCodes.BadRequest, { error: 'No appeal channel found' });
+		}
+
+		const appealChannel = (await this.container.client.channels.fetch(guildData.channelAppeal)) as TextChannel;
+
+		if (!appealChannel) {
+			return response.error(HttpCodes.BadRequest, { error: 'No appeal channel found' });
+		}
+
+		const user = this.container.client.users.cache.get(appeal.userId);
+
+		const accceptButton = new ButtonBuilder()
+			.setCustomId(`@appeal/a/${appeal.id}`)
+			.setDisabled()
+			.setStyle(ButtonStyle.Success)
+			.setLabel('Approve');
+
+		const rejectButton = new ButtonBuilder().setCustomId(`@appeal/r/${appeal.id}`).setDisabled().setStyle(ButtonStyle.Danger).setLabel('Reject');
+
+		const viewModlogsButton = new ButtonBuilder().setCustomId(`@appeal/m/${appeal.id}`).setLabel('View Modlogs').setDisabled();
+
+		appealChannel.send({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(`Appeal #${appeal.idx}`)
+					.setAuthor({ iconURL: user?.displayAvatarURL(), name: `@${user?.username ?? 'dummyuser'}` })
+					.setFields(
+						{
+							name: '1. What type of action are you appealing?',
+							value: [
+								`${appeal.muteOrBan === 'mute' ? CardinalEmojis.Success : CardinalEmojis.Fail} Mute`,
+								`${appeal.muteOrBan === 'ban' ? CardinalEmojis.Success : CardinalEmojis.Fail} Ban`
+							].join('\n')
+						},
+						{
+							name: '2. Why were you moderated?',
+							value: `${appeal.reason}`
+						},
+						{
+							name: '3. What is your appeal?',
+							value: `${appeal.appeal}`
+						},
+						{
+							name: '4. Anything else you would like to add?',
+							value: `${appeal.extra}`
+						}
+					)
+					.setFooter({ text: `User ID: ${appeal.userId}` })
+					.setTimestamp()
+					.setColor(CardinalColors.Info)
+			],
+			components: [new ActionRowBuilder<ButtonBuilder>().addComponents(accceptButton, rejectButton, viewModlogsButton)]
 		});
 
 		return response.status(200).json(appeal);
