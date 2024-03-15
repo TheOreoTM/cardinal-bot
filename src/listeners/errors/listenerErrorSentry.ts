cb8f6b705baf5e1cceab93614f53ea1c5d3707f7
theoreotm <qwertzybad@gmail.com>
2024-03-15 19:01:31 +0500

    feat: event error handling

---
+++ src/listeners/errors/listenerErrorSentry.ts
@@ -0,0 +1,11 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, Listener, type ListenerErrorPayload } from '@sapphire/framework';
+import { captureException } from '@sentry/node';
+import { envIsDefined } from '@skyra/env-utilities';
+
+@ApplyOptions({ enabled: envIsDefined('SENTRY_DSN') })
+export class UserListener extends Listener<typeof Events.ListenerError> {
+	public run(error: Error, context: ListenerErrorPayload) {
+		captureException(error, { tags: { name: context.piece.name } });
+	}
+}
