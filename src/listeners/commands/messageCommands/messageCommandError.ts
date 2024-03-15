c8a97dd5d3a5d98d0e1fa94fd32f500f07b9f761
theoreotm <qwertzybad@gmail.com>
2024-03-15 19:53:12 +0500

    fix: import

--- src/listeners/commands/messageCommands/messageCommandError.ts
+++ src/listeners/commands/messageCommands/messageCommandError.ts
@@ -1,7 +1,7 @@
 import { CardinalEvents } from '#lib/types';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener, type MessageCommandErrorPayload, UserError } from '@sapphire/framework';
-import { handleCommandError } from '../_shared';
+import { handleCommandError } from '../_shared.js';
 
 @ApplyOptions<Listener.Options>({
 	event: CardinalEvents.MessageCommandError
