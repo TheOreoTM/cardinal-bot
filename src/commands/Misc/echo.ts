c8013aea90c96cd62cc709718426557a899a879a
TheOreoTM <qwertzybad@gmail.com>
2024-08-11 15:33:40 +0500

    feat: Echo command

--- src/commands/Misc/echo.ts
+++ src/commands/Misc/echo.ts
@@ -1,3 +1,4 @@
+import { CardinalCommand } from '#lib/structures';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command, type Args } from '@sapphire/framework';
 import { Message } from 'discord.js';
@@ -5,7 +6,7 @@ import { Message } from 'discord.js';
 @ApplyOptions<Command.Options>({
 	name: 'echo'
 })
-export class UserCommand extends Command {
+export class UserCommand extends CardinalCommand {
 	public async messageRun(message: Message, args: Args) {
 		const content = await args.rest('string');
 		return message.reply(content);
