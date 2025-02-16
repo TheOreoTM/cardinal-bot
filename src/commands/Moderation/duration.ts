9eb56e56a472a2d7cc3b1cb10e854fbdf8dce381
TheOreoTM <qwertzybad@gmail.com>
2025-02-16 13:22:51 +0500

    test: duration

--- src/commands/Moderation/duration.ts
+++ src/commands/Moderation/duration.ts
@@ -77,7 +77,7 @@ export class durationCommand extends ModerationCommand {
 			});
 
 			// update task duration
-			const taskList = await this.container.tasks.client.getJobs('active');
+			const taskList = await this.container.tasks.client.getJobs('waiting');
 			console.log(taskList);
 		} catch (ignored) {}
 
